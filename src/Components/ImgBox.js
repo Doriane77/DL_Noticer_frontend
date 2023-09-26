328ff257a647ac950ab4d392e9dc440770422c8b
Doriane Lollia <lollia.doriane@dsfords.fr>
2023-09-26 14:24:13 +0200

    ajout de la nav composant imgBox

--- src/Components/ImgBox.js
+++ src/Components/ImgBox.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../Sass/Components/ImgBox.scss";
-export default function ImgBox({ image, desc }) {
+import { useNavigate } from "react-router-dom";
+export default function ImgBox({ image, desc, navTo }) {
+  let navigate = useNavigate();
   return (
-    <div className="ImgBox">
+    <div className="ImgBox" onClick={() => navigate(navTo)}>
       <img src={image} alt={desc} />
     </div>
   );
