a7846b36fbd6bf5e6ac02fb04983d38552fb0314
Doriane Lollia <lollia.doriane@dsfords.fr>
2023-09-26 16:49:36 +0200

    Création du footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Sass/Footer.scss";
+import ImgBox from "./Components/ImgBox";
+import Logo from "./Assets/Logo.png";
 export default function Footer() {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <ImgBox
+        image={Logo}
+        desc="Lettre N avec un degradé violet bleu"
+        navTo={"/"}
+      />
+      <div className="BoxText">Lollia Doriane - 2023 - </div>
+    </div>
+  );
 }
