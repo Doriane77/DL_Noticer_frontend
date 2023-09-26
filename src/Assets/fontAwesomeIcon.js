1b5e6888d2dfe7a648619bbb908dc68d71305030
Doriane Lollia <lollia.doriane@dsfords.fr>
2023-09-26 15:48:44 +0200

    User Icon

--- src/Assets/fontAwesomeIcon.js
+++ src/Assets/fontAwesomeIcon.js
@@ -1,2 +1,2 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-export const fontAwesomeIcons = { faBars };
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
+export const fontAwesomeIcons = { faBars, faUser };
