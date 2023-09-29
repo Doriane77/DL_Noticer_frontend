426afecbbfb8ca3ba3b1f0c60b98ef9836a4a07e
Doriane Lollia <lollia.doriane@dsfords.fr>
2023-09-29 10:31:03 +0200

    Formulaire user register + changement de formulaire

--- src/Modals/UserForm.js
+++ src/Modals/UserForm.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import useUserStore from "../Stores/useUserStore";
 import "../Sass/Modals/UserForm.scss";
 export default function UserForm() {
-  const { seeModalForms } = useUserStore();
+  const { seeModalForms, openForms } = useUserStore();
   if (!seeModalForms) return null;
   return (
     <div className="UserForm">
-      <LoginForm />
+      {openForms === "login" && <LoginForm />}
+      {openForms === "register" && <RegisterForm />}
     </div>
   );
 }
@@ -14,7 +15,7 @@ function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { loginUser, close, failMessage } = useUserStore();
+  const { loginUser, close, failMessage, changeOpenForms } = useUserStore();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -42,6 +43,59 @@ function LoginForm() {
       <p className="error">{failMessage}</p>
       <button className="submit" type="submit">
         Se connecter
+      </button>
+      <button
+        className="changeForm"
+        onClick={() => changeOpenForms("register")}
+      >
+        Pas de compte ? S'enregistrer{" "}
+      </button>
+    </form>
+  );
+}
+function RegisterForm() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { registerUser, close, failMessage, changeTextForm, changeOpenForms } =
+    useUserStore();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    changeTextForm();
+    registerUser(userName, email, password);
+  };
+  return (
+    <form className="RegisterForm" onSubmit={handleSubmit}>
+      <button className="X" type="button" onClick={close}>
+        X
+      </button>
+      <input
+        type="text"
+        aria-label="Votre nom d'utilisateur"
+        placeholder="Nom d'utilisateur"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="email"
+        aria-label="Votre adresse email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        aria-label="Votre mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <p className="error">{failMessage}</p>
+      <button className="submit" type="submit">
+        Se connecter
+      </button>
+      <button className="changeForm" onClick={() => changeOpenForms("login")}>
+        Déja un compte ? Se connecter
       </button>
     </form>
   );
--- src/Stores/useUserStore.js
+++ src/Stores/useUserStore.js
@@ -4,11 +4,33 @@ import { create } from "zustand";
 const useUserStore = create((set) => ({
   user: null,
   seeModalForms: false,
+  openForms: "login",
   failMessage: null,
+  changeOpenForms: (nameForm) => set(() => ({ openForms: nameForm })),
+  changeTextForm: () => set(() => ({ failMessage: null })),
   setseeModalForms: () =>
     set((state) => ({ seeModalForms: !state.seeModalForms })),
   open: () => set(() => ({ seeModalForms: true })),
-  close: () => set(() => ({ seeModalForms: false })),
+  close: () =>
+    set(() => ({
+      seeModalForms: false,
+      failMessage: null,
+      openForms: "login",
+    })),
+  registerUser: async (username, email, password) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_URL}/user/register`,
+        { username, email, password }
+      );
+      set({ user: response.data, failMessage: null, seeModalForms: false });
+    } catch (error) {
+      console.log("error: ", error);
+      set({
+        failMessage: error.response.data.message,
+      });
+    }
+  },
   loginUser: async (email, password) => {
     try {
       const response = await axios.post(
