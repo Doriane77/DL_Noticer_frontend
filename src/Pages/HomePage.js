61bfe791f9469f34e162108fd68f794ee401031a
Doriane Lollia <lollia.doriane@dsfords.fr>
2023-10-05 17:12:24 +0200

    home page + ajout d'un font roboto et poppins

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,30 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../Sass/Pages/HomePage.scss";
-import axios from "axios";
-import useHeaderStore from "../Stores/Header";
 
 export default function HomePage() {
-  const [dataMovies, setDataMovies] = useState([]);
-  const close = useHeaderStore((state) => state.close);
-
-  useEffect(() => {
-    close();
-    const fetchMovies = async () => {
-      try {
-        const requete = await axios.get(
-          `${process.env.REACT_APP_URL}/movie/all`
-        );
-        setDataMovies(requete.data);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des données", error);
-      }
-    };
-    fetchMovies();
-  }, [close]);
   return (
     <div className="HomePage">
-      <section>
-        <article>films</article>
+      <h1>NOTICER</h1>
+
+      <section className="home-welcome">
+        <h2>
+          Bienvenue sur <span>Noticer</span>
+        </h2>
+        <p className="subtitle">
+          Votre espace de critiques
+          <br />
+          cinématographiques et littéraires
+        </p>
+      </section>
+
+      <section className="home-section">
+        <h2>Qui sommes-nous ?</h2>
+        <p>
+          <strong>Noticer</strong> est votre compagnon
+          <br />
+          idéal dans le vaste univers
+          <br />
+          du cinéma et de la littérature.
+        </p>
+      </section>
+
+      <section className="home-section">
+        <h2>Que vous offrons-nous ?</h2>
+        <p>
+          Embarquez dans un voyage
+          <br />
+          à travers notre riche bibliothèque
+          <br />
+          de critiques pour enrichir
+          <br />
+          vos expériences cinématographiques
+          <br />
+          et littéraires.
+        </p>
+      </section>
+
+      <section className="home-section">
+        <h2>Comment optimiser votre expérience sur Noticer ?</h2>
+        <p>
+          Le monde de <strong>Noticer</strong> est ouvert
+          <br />
+          à tous. Lisez les critiques qui
+          <br />
+          vous intriguent, et pour une
+          <br />
+          participation active, un simple
+          <br />
+          enregistrement suffit.
+        </p>
+      </section>
+
+      <section className="home-section">
+        <h2>Prêt pour l'aventure ?</h2>
+        <p>
+          Rejoignez notre communauté passionnée
+          <br />
+          dès aujourd'hui et enrichissez votre univers
+          <br />
+          cinématographique et littéraire.
+        </p>
       </section>
     </div>
   );
